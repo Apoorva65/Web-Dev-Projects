94847ed6b31c2d5ebe9d8e8e21c93f8e9023514e
Apoorva65 <apoorvasdixit@gmail.com>
2025-12-01 15:34:08 +0530

    Create Currency Converter project and add basic components

---
+++ CurrencyConverter/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+import './components/InputBox.jsx'
+import InputBox from './components/InputBox.jsx'
+
+
+function App() {
+
+  return (
+  <>
+  <h1>Currency converter</h1>
+  <InputBox />
+  </>
+  )
+}
+
+export default App
---
+++ CurrencyConverter/src/hooks/useCurrencyInfo.js
@@ -0,0 +1,14 @@
+import { useState,useCallback,useEffect } from 'react'
+
+function useCurrencyInfo(currency){
+    const [data,setData] = useState({})
+    useEffect(() =>{
+        let url = `https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/${currency}.json`;
+        fetch(url)
+        .then((res)=>res.json())
+        .then((res) => setData(res[currency]))
+    },[currency])
+    return data;
+}
+
+export default useCurrencyInfo;
