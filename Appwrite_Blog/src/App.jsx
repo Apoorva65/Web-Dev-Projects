bf3dac0b65cb340b432252944f4af3a54a8a2dd5
Apoorva65 <apoorvasdixit@gmail.com>
2026-01-04 17:00:12 +0530

    Create The blog project using appwrite as backend and add authentication

---
+++ Appwrite_Blog/src/App.jsx
@@ -0,0 +1,14 @@
+import conf from './conf/conf'
+import './App.css'
+
+function App() {
+  console.log(conf.appwriteUrl);
+
+  return (
+    <>
+      <h1>Hi All the best</h1>
+    </>
+  )
+}
+
+export default App
