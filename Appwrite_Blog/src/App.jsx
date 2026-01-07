126a7d72b141e3c2774783a5e26fd19b04fb7ced
Apoorva65 <apoorvasdixit@gmail.com>
2026-01-07 20:32:53 +0530

    Configure store and reducers for the blog

--- Appwrite_Blog/src/App.jsx
+++ Appwrite_Blog/src/App.jsx
@@ -1,13 +1,38 @@
 import conf from './conf/conf'
 import './App.css'
+import { useEffect, useState } from 'react'
+import {useDispatch} from 'react-redux'
+import authServicee from './appwrite/auth'
+import { login,logout } from './store/authSlice'
+import Header from './components/Header/Header.jsx'
+import Footer from './components/Footer/Footer.jsx'
+
 
 function App() {
-  console.log(conf.appwriteUrl);
 
-  return (
-    <>
-      <h1>Hi All the best</h1>
-    </>
+  const [loading,setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(()=>{
+    authServicee.getCurrentUser()
+    .then((userData)=>{
+      if(userData){
+        dispatch(login({userData}))
+      }
+      else{
+        dispatch(logout())
+      }
+    })
+    .finally(()=>{setLoading(false)})
+  },[])
+
+  return loading ? (null) : (
+    <div className='min-h-screen flex flex-wrap content-between'>
+      <div className='w-full block'>
+        <Header/>
+        <Footer/>
+      </div>
+    </div>
   )
 }
 
